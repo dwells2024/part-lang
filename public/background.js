46ce4c3da4608650d094ff97d050c0f7df208104
dwells2024 <davidwells2024@u.northwestern.edu>
2024-02-08 10:39:20 -0600

    updated background to get text content on all pages

--- public/background.js
+++ public/background.js
@@ -2,25 +2,16 @@
 
 console.log("background working");
 
-// Declaring constants to store LinkedIn URLs.
-const linkedInListViewURL = "https://www.linkedin.com/jobs/collections";
-const linkedInDetailView = "https://www.linkedin.com/jobs/view";
-
-// This function returns the appropriate job description class name based on the URL.
-function getJobDescriptionClassName(url) {
-  // If the URL starts with the LinkedIn list view URL, return the list view class name, else return the detail view class name.
-  return url.startsWith(linkedInListViewURL)
-    ? "jobs-search__job-details--container"
-    : "jobs-description-content__text";
-}
-
-// This function grabs the job description text from the web page.
-function grabJobDescription(className) {
-  const jobDetailsContainer = document.body.querySelector(`.${className}`);
-  const jobDetails = jobDetailsContainer.textContent;
-  const cleanedJobDetails = jobDetails.replace(/\s\s+/g, " ");
-  console.log(cleanedJobDetails);
-  return cleanedJobDetails;
+function grabText() {
+  console.log("grabbing text");
+  const textContainers = document.body.querySelectorAll('p');
+  textContent = ""
+  textContainers.forEach(function (p) {
+    textContent += p.textContent;
+  })
+  const cleanedText = textContent.replace(/\s\s+/g, " ");
+  console.log(cleanedText);
+  return cleanedText;
 }
 
 // This is an event listener that runs when a tab is updated in Chrome.
@@ -28,22 +19,14 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   console.log("listener triggered");
   // Check if the tab is fully loaded and active.
   if (changeInfo.status === "complete" && tab.active) {
-    // Check if the URL of the tab matches the LinkedIn list or detail view URL.
-    if (
-      tab.url?.startsWith(linkedInListViewURL) ||
-      tab.url?.startsWith(linkedInDetailView)
-    ) {
-      // Execute the grabJobDescription function on the current tab and store the result in local storage.
-      chrome.scripting
-        .executeScript({
-          target: { tabId: tabId },
-          func: grabJobDescription,
-          args: [getJobDescriptionClassName(tab.url)],
-        })
-        .then((queryResult) => {
-          chrome.storage.local.set({ jobDescription: queryResult[0].result });
-          console.log("stored description")
-        });
-    }
+    chrome.scripting
+      .executeScript({
+        target: { tabId: tabId },
+        func: grabText,
+      })
+      .then((queryResult) => {
+        chrome.storage.local.set({ pTextContent: queryResult[0].result })
+        console.log(queryResult[0].result);
+      })
   }
 });
--- src/utils/chatGPTUtil.js
+++ src/utils/chatGPTUtil.js
@@ -3,6 +3,16 @@ import axios from "axios";
 const CHATGPT_END_POINT = "https://api.openai.com/v1/chat/completions";
 const CHATGPT_MODEL = "gpt-3.5-turbo";
 
+// Potential message format
+
+// Your job is to identify any phrases that have partisan bias. Respond only with a JSON object with and object for each phrase found in the format:
+// {
+// phrase: "the phrase with bias",
+// reason: "the reason it its biased",
+// suggestion: "neutral suggestion"
+// }
+// Analyze the following text:
+
 export const postChatGPTMessage = async (message, openAIKey) => {
     const config = {
         headers: {
