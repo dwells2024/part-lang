a552628b2f72ce72e86e11557d2892dec40f5969
dwells2024 <davidwells2024@u.northwestern.edu>
2024-02-08 11:09:08 -0600

    fixed settings import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Home from './components/Home';
+import Settings from './components/Settings';
 import { ROUTES } from './utils/routes';
 import { loadData } from './utils/localStorage';
 
